48deeefd57f5700751302c02a1dbb97db501c125
avielmoshe <mosheavieli@gmail.com>
2024-11-20 16:45:59 +0200

    1

--- src/components/cardPage/CardPage.jsx
+++ src/components/cardPage/CardPage.jsx
@@ -1,6 +1,6 @@
 import { getOne } from "../../utils/getApi.js";
 import { useState, useEffect } from "react";
-import styles from "./CardPage.module.css";
+import "./CardPage.css";
 
 const CardPage = ({ userChoice }) => {
   const [Data, setData] = useState(null);
@@ -17,15 +17,27 @@ const CardPage = ({ userChoice }) => {
 
   return (
     Data && (
-      <div className={styles.pokemonCard}>
+      <div className="pokemonCard">
         <h1>{Data.forms[0].name}</h1>
-        <img src={Data.sprites.other.dream_world.front_default} alt="" />
+        <img
+          src={Data.sprites.other.dream_world.front_default}
+          alt={Data.forms[0].name}
+        />
+        <p>Type: {Data.types[0].type.name}</p>
         <p>Ability: {Data.abilities[0].ability.name}</p>
+        <h2>Pokemon Voice</h2>
         <audio controls>
           <source src={Data.cries.latest} type="audio/mp3" />
-          <source src={Data.cries.latest} type="audio/ogg" />
           Your browser does not support the audio element.
         </audio>
+        <p className="base_experience">Experience: {Data.base_experience}</p>
+        <p>Favorite Move: {Data.moves[0].move.name}</p>
+        <h2>In Action!</h2>
+        <img
+          className="gif"
+          src={Data.sprites.other.showdown.front_default}
+          alt={Data.forms[0].name}
+        />
       </div>
     )
   );
