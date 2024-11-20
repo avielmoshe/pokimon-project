67ac27739a61e60ceceee8a74be57c5187463b49
MaorShmueli55 <maorshmueli5@gmail.com>
2024-11-20 14:02:55 +0200

    test

--- src/components/cardPage/CardPage.jsx
+++ src/components/cardPage/CardPage.jsx
@@ -1,5 +1,6 @@
 import { getOne } from "../../utils/getApi.js";
 import { useState, useEffect } from "react";
+import styles from "./HomePage.module.css";
 
 const CardPage = ({ userChoice }) => {
   const [Data, setData] = useState(null);
