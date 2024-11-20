5a8fd5f666b748eb5bf89107d34354bab6b5fddd
avielmoshe <mosheavieli@gmail.com>
2024-11-20 16:04:52 +0200

    1

--- src/components/cardPage/CardPage.jsx
+++ src/components/cardPage/CardPage.jsx
@@ -1,5 +1,6 @@
 import { getOne } from "../../utils/getApi.js";
 import { useState, useEffect } from "react";
+import styles from "./CardPage.module.css";
 
 const CardPage = ({ userChoice }) => {
   const [Data, setData] = useState(null);
@@ -16,8 +17,15 @@ const CardPage = ({ userChoice }) => {
 
   return (
     Data && (
-      <div>
-        <img src={Data.sprites.other.dream_world.front_default} alt="" /> ;
+      <div className={styles.pokemonCard}>
+        <h1>{Data.forms[0].name}</h1>
+        <img src={Data.sprites.other.dream_world.front_default} alt="" />
+        <p>Ability: {Data.abilities[0].ability.name}</p>
+        <audio controls>
+          <source src={Data.cries.latest} type="audio/mp3" />
+          <source src={Data.cries.latest} type="audio/ogg" />
+          Your browser does not support the audio element.
+        </audio>
       </div>
     )
   );
