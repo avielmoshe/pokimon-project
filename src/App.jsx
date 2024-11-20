fa05aaa3ca4b13498a696623249f57f2c0d3062e
avielmoshe <mosheavieli@gmail.com>
2024-11-20 14:25:02 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import MainConteiner from "./components/mainConteiner/MainConteiner.jsx";
+import DrawerAppBar from "./components/NavBar.jsx";
 // import NavBar from "./components/mainConteiner/MainConteiner.jsx";
 
 function App() {
   return (
     <>
+      <DrawerAppBar />
       <MainConteiner />
     </>
   );
