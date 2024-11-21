f328196f545c3892c25a1ce9296fb99e12a91dda
MaorShmueli55 <maorshmueli5@gmail.com>
2024-11-21 14:18:27 +0200

    1

---
+++ src/components/abuot/About.jsx
@@ -0,0 +1,16 @@
+import "./About.module.css"
+
+const About = () =>{
+    return(
+        <div className="about">
+
+           About Us
+           Welcome to our Pokémon Cards website! This platform was created as part of our Software Development course, combining our passion for coding with our love for the magical world of Pokémon. Here, you can explore a vast collection of Pokémon cards, each showcasing unique designs, stats, and abilities that bring the Pokémon universe to life. One of our key features is the ability to add your favorite cards to a personalized favorites list, making it easier than ever to keep track of the Pokémon that inspire you the most.
+           
+           This project was lovingly crafted by Maor Shmueli and Aviel Moshe, two aspiring developers who believe in the power of creativity, teamwork, and technology. Our goal is to provide a fun and engaging experience for Pokémon fans and card collectors alike. Whether you're revisiting your childhood nostalgia or discovering Pokémon for the first time, we hope our website brings you joy and a sense of adventure.
+           
+           Dive in, explore, and enjoy the thrill of collecting your favorite Pokémon cards! This is more than just a project for us—it's a celebration of creativity and innovation. Thank you for visiting, and feel free to share your thoughts or feedback to help us improve!
+        </div>
+    )
+}
+export default About;
